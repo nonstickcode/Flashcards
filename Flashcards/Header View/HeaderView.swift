37ef0a4aa7986180b17b868b540dbc0b9d3a8919
Cody <66153585+nonstickcode@users.noreply.github.com>
2023-11-26 14:53:59 -0700

    add settings gear icon to header

--- Flashcards/Header View/HeaderView.swift
+++ Flashcards/Header View/HeaderView.swift
@@ -10,12 +10,26 @@ import SwiftUI
 struct HeaderView: View {
     var body: some View {
         HStack {
+            Spacer()
+                .frame(width: 30, height: 30)
+                .padding(.leading, 10)
+            
             Text("Mathcards")
                 .font(.largeTitle)
                 .bold()
+                .frame(maxWidth: .infinity, alignment: .center) // Align text to the left
+
+            Image(systemName: "gear")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 30, height: 30)
+                .foregroundColor(.white)
+                .padding(.trailing, 10) // Adjust the trailing padding
         }
+        .padding() // Add padding to the HStack if needed
     }
 }
+
 
 #Preview {
     HeaderView()
